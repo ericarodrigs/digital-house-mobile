6874e02a57c66d5cbba351f56e89abca9d14a855
Erica Rodrigues <ericamrodrigs@gmail.com>
2020-09-14 09:34:41 -0300

    do while e funcoes

--- aula-06/exercicio-aula-02/src/nivel01/exercicio-04.kt
+++ aula-06/exercicio-aula-02/src/nivel01/exercicio-04.kt
@@ -14,38 +14,55 @@ import kotlin.random.Random
 */
 
 fun main() {
-    println("Rolar dados? (s ou n)")
-    val jogar = readLine()!!.toLowerCase()
+    do {
+        println("Rolar dados? (s ou n)")
+        val jogar = readLine()!!.toLowerCase()
 
-    if (jogar == "s") {
-        when (val valorDado = Random.nextInt(2, 12)) {
-            7, 11 -> {
-                println("Você tirou $valorDado! Parabéns você ganhou!")
+        if (jogar == "s") {
+            when (val valorDado = valorDoDado()) {
+                7, 11 -> {
+                    mensagemDeGanhou(valorDado)
+                }
+                2, 3, 12 -> {
+                    mensagemDePerdeu(valorDado)
+                }
+                4, 5, 6, 8, 9, 10 -> {
+                    jogarNovamente(valorDado, valorDado)
+                }
             }
-            2, 3, 12 -> {
-                println("Você tirou $valorDado! Você perdeu")
-            }
-            4, 5, 6, 8, 9, 10 -> {
-                jogarNovamente(valorDado, valorDado)
-            }
+        } else {
+            println("O jogo acabou")
+            break
         }
-    } else {
-        println("O jogo acabou")
-    }
+    } while (true)
 }
 
 fun jogarNovamente(valorTirado: Int, valorAuxiliar: Int) {
     print("Você tirou $valorAuxiliar! Aperte enter para tentar novamente")
-    readLine();
-    when (val valorDado = Random.nextInt(2, 12)) {
+    readLine()
+    when (val valorDado = valorDoDado()) {
         7 -> {
-            println("Você tirou 7! Você perdeu =(")
+            mensagemDePerdeu(valorDado)
         }
         valorTirado -> {
-            println("Você tirou $valorDado! Parabéns você ganhou!")
+            mensagemDeGanhou(valorDado)
         }
         else -> jogarNovamente(valorTirado, valorDado)
     }
 
+}
+
+private fun valorDoDado(): Int {
+    return Random.nextInt(2, 12)
+}
+
+private fun mensagemDeGanhou(valorDado: Int) {
+    println("Você tirou $valorDado! Parabéns você ganhou!")
+    println()
+}
+
+private fun mensagemDePerdeu(valorDado: Int) {
+    println("Você tirou $valorDado! Você perdeu =(")
+    println()
 }
 
