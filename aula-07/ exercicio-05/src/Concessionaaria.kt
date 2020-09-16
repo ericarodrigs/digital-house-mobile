dcd0617c1ccb2a7372e79377922aeeab3d85af82
Erica Rodrigues <ericamrodrigs@gmail.com>
2020-09-16 19:27:46 -0300

    corrigindo exercicio 5

--- aula-07/ exercicio-05/src/Concessionaaria.kt
+++ aula-07/ exercicio-05/src/Concessionaaria.kt
@@ -1,6 +1,13 @@
+import java.util.ArrayList
+
 class Concessionaaria {
+
+    var registroDeVendas = ArrayList<Venda>()
+
     fun registrarVenda(veiculo: Veiculo, cliente: Cliente, valor: Double) {
         val venda = Venda(valor, veiculo, cliente)
+        registroDeVendas.add(venda)
         println("Venda = ${venda.cliente.nome} comprou um ${venda.veiculoVendido.modelo} por ${venda.valorDaVenda}")
+        println("Você tem ${registroDeVendas.size} vendas")
     }
 }
